5b860b1c58464aa2002c5e8ecd46ba2f77cd9f1d
Khristian Marcial <KhristianMarcial@gmail.com>
2020-02-29 16:43:03 -0500

    adds profile

---
+++ src/pages/Campaigns/Campaigns.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Campaigns.module.css';
+
+const Campaigns = (props) => {
+    return (
+        <main>
+            <h1>Campaigns</h1>
+        </main>
+    );
+};
+
+export default Campaigns;
