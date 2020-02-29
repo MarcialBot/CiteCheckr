530eb451d8b612a7dc279699b52906824c379f57
Khristian Marcial <KhristianMarcial@gmail.com>
2020-02-29 16:44:57 -0500

    adds login and signup forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Switch, Route }  from 'react-router-dom';
+
+//Presentational components
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+//Page components
+import Home from './pages/Home/Home';
+import Campaigns from './pages/Campaigns/Campaigns';
+import Profile from './pages/Profile/Profile';
 import './App.css';
 
 class App extends React.Component {
@@ -14,8 +23,19 @@ class App extends React.Component {
     <div className="App-outer-container">
         <Navbar />
         <div className="App-inner-container">
-          
+          <Switch> 
+            <Route exact path ='/' render={props => 
+              <Home />
+            } />
+            <Route exact path ='/campaigns' render={props => 
+              <Campaigns />
+            } />
+            <Route exact path ='/profile' render={props => 
+              <Profile />
+            } />
+          </Switch>
         </div>
+        <Footer />
     </div>
   );
     }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './Navbar.module.css'
+import { Link } from 'react-router-dom';
 
 const Navbar = (props) => {
     return (
         <nav className={styles.navbar}>
-            <h1>CiteCheckr</h1>
+            <Link to ='/'>
+                <h1>CiteCheckr</h1>
+            </Link>
             <ul>
-                <li>Profile</li>
+                <li>
+                    <Link to ='/Campaigns'>Campaigns</Link>
+                </li>
+                <li>
+                    <Link to ='/Profile'>Profile</Link>
+                </li>
                 <li>Login</li>
                 <li>Sign Up</li>
             </ul>
